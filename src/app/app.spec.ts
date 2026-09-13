0903692c2961dca619d1bfdebe3c78b6324b29ae
Miloš Krstić <mrkrsticmilos@gmail.com>
2026-09-13 20:49:15 +0200

    AI styling

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,8 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, NG Switzerland by Grizli Zli',
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('AI. ON YOURTERMS.');
   });
 });
